0f3b119d8ad656cb9364c93ae808871ba3b0a946
Jonathan <jlc3939@gmail.com>
2023-09-12 19:08:13 -0400

    fixes

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,9 +8,9 @@ import { Slide } from '../carousel/slide';
 })
 export class ProjectsComponent implements OnInit {
   public slides: Slide[] = [
-    {url: "https://placehold.co/600x400/orange/white", alt: "First Slide", text: "At this event I did X" },
-    {url: "https://placehold.co/600x400/red/black", alt: "Second Slide", text: "At this event I did Y" },
-    {url: "https://placehold.co/600x400/000000/AAA", alt:"Third Image", text: "At this event I did Z" }];
+    {url: "https://placehold.co/800x600/orange/white", alt: "First Slide", text: "At this event I did X" },
+    {url: "https://placehold.co/800x600/red/black", alt: "Second Slide", text: "At this event I did Y" },
+    {url: "https://placehold.co/800x600/000000/AAA", alt:"Third Image", text: "At this event I did Z" }];
   
   constructor() { 
 
